576ad74158a07d709b75c63b61b157ae8399aef7
muratmaga <muratmaga@outlook.com>
2020-07-30 21:35:20 -0700

    Update ALPACA.py
    with clearer help instructions

--- ALPACA/ALPACA.py
+++ ALPACA/ALPACA.py
@@ -50,7 +50,7 @@ class ALPACA(ScriptedLoadableModule):
     self.parent.dependencies = []
     self.parent.contributors = ["Arthur Porto, Sara Rolfe (UW), Murat Maga (UW)"] # replace with "Firstname Lastname (Organization)"
     self.parent.helpText = """
-      This module transfers landmarks from one image to a directory of images.
+      This module automatically transfers landmarks on a reference 3D model (mesh) to a target 3D model using dense correspondence and deformable registration. First optimize the parameters in pairwise analysis, then use the batch mode to apply to all 3D models
       """
     self.parent.helpText += self.getDefaultModuleDocumentationLink()
     self.parent.acknowledgementText = """
