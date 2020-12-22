c4f00639ba4742d53f99cd630389ced7c56b0fce
arpinerap <arpinerap@gmail.com>
2020-12-22 17:38:10 +0100

    Update ExportAs.py

--- ExportAs/ExportAs.py
+++ ExportAs/ExportAs.py
@@ -38,18 +38,18 @@ class ExportAsSubjectHierarchyPlugin(AbstractScriptedSubjectHierarchyPlugin):
     AbstractScriptedSubjectHierarchyPlugin.__init__(self, scriptedPlugin)
 
     self.exportAsAction = qt.QAction(f"Export as...", scriptedPlugin)
-    self.exportTfmAsAction = qt.QAction(f"Export Hardened as...", scriptedPlugin)
+    self.exportTransformedAsAction = qt.QAction(f"Export transformed as...", scriptedPlugin)
     self.menu = qt.QMenu("Plugin Menu")
-    self.menu2 = qt.QMenu("Plugin Menu")
+    self.transformedMenu = qt.QMenu("Transformed Plugin Menu")
     self.exportAsAction.setMenu(self.menu)
-    self.exportTfmAsAction.setMenu(self.menu2)
+    self.exportTransformedAsAction.setMenu(self.transformedMenu)
 
   #
   # item context menus are what happens when you right click on a selected line
   #
   def itemContextMenuActions(self):
     """the actions that could be shown for any of this plugins items"""
-    return [self.exportAsAction]
+    return [self.exportAsAction, self.exportTransformedAsAction]
 
   def showContextMenuActionsForItem(self, itemID):
     """Set actions visible that are valid for this itemID"""
@@ -57,25 +57,41 @@ class ExportAsSubjectHierarchyPlugin(AbstractScriptedSubjectHierarchyPlugin):
     subjectHierarchyNode = pluginHandlerSingleton.subjectHierarchyNode()
     self.exportAsAction.visible = True
     self.exportAsAction.enabled = False
-    self.exportTfmAsAction.visible = True
-    self.exportTfmAsAction.enabled = False
+    self.exportTransformedAsAction.visible = False
+    self.exportTransformedAsAction.enabled = False
     self.menu.clear()
-    self.menu2.clear()
+    self.transformedMenu.clear()
     associatedNode = subjectHierarchyNode.GetItemDataNode(itemID)
     if associatedNode is not None and associatedNode.IsA("vtkMRMLStorableNode"):
         self.exportAsAction.enabled = True
         writerExtensions = slicer.app.coreIOManager().fileWriterExtensions(associatedNode)
         for writerFormat in writerExtensions:
           a = self.menu.addAction(writerFormat)
-          a.connect("triggered()", lambda writerFormat=writerFormat : self.export(associatedNode, writerFormat)))  
+          a.connect("triggered()", lambda writerFormat=writerFormat : self.export(associatedNode, writerFormat, False))
+    
     if associatedNode is not None and associatedNode.IsA("vtkMRMLStorableNode") and associatedNode.GetTransformNodeID():
-        self.exportTfmAsAction.enabled = True
+        self.exportTransformedAsAction.visible = True
+        self.exportTransformedAsAction.enabled = True
         writerExtensions = slicer.app.coreIOManager().fileWriterExtensions(associatedNode)
         for writerFormat in writerExtensions:
-          b = self.menu2.addAction(writerFormat)
-          b.connect("triggered()", lambda writerFormat=writerFormat : self.exportHardened(associatedNode, writerFormat))
+          b = self.transformedMenu.addAction(writerFormat)
+          b.connect("triggered()", lambda writerFormat=writerFormat : self.export(associatedNode, writerFormat, True))
 
-  def export(self, node, writerFormat):
+  def export(self, node, writerFormat, transformedFlag):
+    if transformedFlag:
+      shNode = slicer.vtkMRMLSubjectHierarchyNode.GetSubjectHierarchyNode(slicer.mrmlScene)
+      itemIDToClone = shNode.GetItemByDataNode(node)
+      clonedItemID = slicer.modules.subjecthierarchy.logic().CloneSubjectHierarchyItem(shNode, itemIDToClone)
+      clonedNode = shNode.GetItemDataNode(clonedItemID)
+
+      transformNode = slicer.mrmlScene.GetNodeByID(node.GetTransformNodeID())
+      clonedNode.SetAndObserveTransformNodeID(transformNode.GetID())
+      
+      transformLogic = slicer.vtkSlicerTransformLogic()
+      transformLogic.hardenTransform(clonedNode)
+
+      node = clonedNode
+    
     fileName = qt.QFileDialog.getSaveFileName(slicer.util.mainWindow(),
                                             "Export As...", node.GetName(), writerFormat)
     extension = slicer.vtkDataFileFormatHelper.GetFileExtensionFromFormatString(writerFormat)
@@ -89,37 +105,9 @@ class ExportAsSubjectHierarchyPlugin(AbstractScriptedSubjectHierarchyPlugin):
       logging.info(f"Exported {node.GetName()} to {fileName}")
     else:
       slicer.util.errorDisplay(f"Could not save {node.GetName()} to {fileName}")
-  
-  def exportHardened(self, node, writerFormat):
-    # Clone model node
-    shNode = slicer.vtkMRMLSubjectHierarchyNode.GetSubjectHierarchyNode(slicer.mrmlScene)
-    itemIDToClone = shNode.GetItemByDataNode(node)
-    clonedItemID = slicer.modules.subjecthierarchy.logic().CloneSubjectHierarchyItem(shNode, itemIDToClone)
-    print (clonedItemID)
-    clonedNode = shNode.GetItemDataNode(clonedItemID)
-    
-    # Apply model node transform to cloned node
-    trfNode = slicer.mrmlScene.GetNodeByID(node.GetTransformNodeID())
-    clonedNode.SetAndObserveTransformNodeID(trfNode.GetID())
 
-    # Harden cloned model transform
-    transformLogic = slicer.vtkSlicerTransformLogic()
-    transformLogic.hardenTransform(clonedNode)
+    if transformedFlag:
+      slicer.mrmlScene.RemoveNode(clonedNode.GetStorageNode())
+      slicer.mrmlScene.RemoveNode(clonedNode.GetDisplayNode())
+      slicer.mrmlScene.RemoveNode(clonedNode)
 
-    # Save cloned model to file
-    fileName = qt.QFileDialog.getSaveFileName(slicer.util.mainWindow(),
-                                            "Export As...", clonedNode.GetName(), writerFormat)
-    extension = slicer.vtkDataFileFormatHelper.GetFileExtensionFromFormatString(writerFormat)
-    if not fileName.endswith(extension):
-        fileName = fileName + extension
-    if fileName == "":
-      return
-    writerType = slicer.app.coreIOManager().fileWriterFileType(clonedNode)
-    success = slicer.app.coreIOManager().saveNodes(writerType, {"nodeID": clonedNode.GetID(), "fileName": fileName})
-    if success:
-      logging.info(f"Exported {clonedNode.GetName()} to {fileName}")
-    else:
-      slicer.util.errorDisplay(f"Could not save {clonedNode.GetName()} to {fileName}")
-
-    # Remove cloned model Node
-    slicer.mrmlScene.RemoveNode(clonedNode)
