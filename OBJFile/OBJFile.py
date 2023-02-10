ec7f93087c4a67380e20db027e2b50b2175cb79e
Chi Zhang <80793828+chz31@users.noreply.github.com>
2023-02-10 11:43:19 -0800

    update format

--- OBJFile/OBJFile.py
+++ OBJFile/OBJFile.py
@@ -64,25 +64,25 @@ class OBJFileFileReader(object):
       obj_filename = os.path.basename(obj_path)
       base_name = os.path.splitext(obj_filename)[0]
       extension = os.path.splitext(obj_filename)[1]
-      
+
       #Add model node
       obj_node = slicer.util.loadModel(obj_path)
-      
+
       #Meanwhie, if the model node ends in obj, search and map texture to it
       if extension == ".obj":
         mtl_filename = base_name + ".mtl"
         mtl_path = os.path.join(obj_dir, mtl_filename)
-        
+
         #parse the mtl file to get texture image file name
         if os.path.exists(mtl_path):
           with open(mtl_path) as f:
             lines = f.read().splitlines()
-            
+   
         lines = [i for i in lines if i!='']
-        
+
         texture_filename = lines[len(lines)-1].split(" ")[1]
         texture_path = os.path.join(obj_dir, texture_filename)
-        
+
         import ImageStacks
         logic = ImageStacks.ImageStacksLogic()
         vectorVolNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLVectorVolumeNode")
@@ -91,7 +91,7 @@ class OBJFileFileReader(object):
         logic.outputGrayscale = False
         logic.filePaths = [texture_path]
         logic.loadVolume(outputNode=vectorVolNode, progressCallback = None)
-        
+
         #Map texture to the imported OBJ file
         modelDisplayNode = obj_node.GetDisplayNode()
         modelDisplayNode.SetBackfaceCulling(0)
