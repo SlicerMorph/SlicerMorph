51b1d30a870dfa2aebdddf5365e0da9b7c255b25
oshane <oothomas07@gmail.com>
2024-05-10 08:23:59 -0700

    avoiding viewnode logic to render original scene.

--- HiResScreenCapture/HiResScreenCapture.py
+++ HiResScreenCapture/HiResScreenCapture.py
@@ -227,8 +227,9 @@ class HiResScreenCaptureLogic(ScriptedLoadableModuleLogic):
 
             # Set the layout to include the necessary view
             layoutManager.setLayout(slicer.vtkMRMLLayoutNode.SlicerLayoutDualMonitorFourUpView)
-            viewLogic = slicer.app.applicationLogic().GetViewLogicByLayoutName("1+")
-            viewNode = viewLogic.GetViewNode()
+            viewNode = layoutManager.threeDWidget(0).mrmlViewNode()
+            # viewLogic = slicer.app.applicationLogic().GetViewLogicByLayoutName("1+")
+            # viewNode = viewLogic.GetViewNode()
             layoutManager.addMaximizedViewNode(viewNode)
             newCamera = slicer.modules.cameras.logic().GetViewActiveCameraNode(viewNode)
 
