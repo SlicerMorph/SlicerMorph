76f5468814761184cc4b98891224e9b5c99d86cc
Sara Rolfe <sara.rolfe@seattlechildrens.org>
2024-07-25 01:04:14 -0700

    Preserve volume rendering properties in QuickAlign.py
    If a volume has been rendered previously, reuse the existing volume rendering properties.

--- QuickAlign/QuickAlign.py
+++ QuickAlign/QuickAlign.py
@@ -298,8 +298,11 @@ class QuickAlignWidget(ScriptedLoadableModuleWidget, VTKObservationMixin):
 
         for i, currentNode in enumerate(nodeList):
           if currentNode.GetNodeTagName() == "Volume":
-            displayNode = volRenLogic.CreateDefaultVolumeRenderingNodes(currentNode)
-            displayNode.GetVolumePropertyNode().Copy(volRenLogic.GetPresetByName("US-Fetal"))
+            if currentNode.GetNumberOfDisplayNodes() > 1:
+              displayNode = currentNode.GetNthDisplayNode(1)
+            else:
+              displayNode = volRenLogic.CreateDefaultVolumeRenderingNodes(currentNode)
+              displayNode.GetVolumePropertyNode().Copy(volRenLogic.GetPresetByName("US-Fetal"))
           else:
             displayNode=currentNode.GetDisplayNode()
           displayNode.SetVisibility(True)
